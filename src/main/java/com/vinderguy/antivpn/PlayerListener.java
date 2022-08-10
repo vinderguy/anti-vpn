1da8b6a8f5f49da688c98fa3a63fbbc808a0bc74
Vinderguy <vinderguy@gmail.com>
2022-08-10 21:52:46 +0200

    Add PlayerListener Class

---
+++ src/main/java/com/vinderguy/antivpn/PlayerListener.java
@@ -0,0 +1,22 @@
+package com.vinderguy.antivpn;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.jetbrains.annotations.NotNull;
+
+public final class PlayerListener implements Listener {
+
+    private final AntiVPN _plugin;
+
+    public PlayerListener(@NotNull final AntiVPN plugin) {
+        _plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerLogin(@NotNull final AsyncPlayerPreLoginEvent e) {
+        if (_plugin.isIPBlocked(e.getHostname())) e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, Component.text(_plugin.getMessage("connection-blocked")));
+    }
+}
