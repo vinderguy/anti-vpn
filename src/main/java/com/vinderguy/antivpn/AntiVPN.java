852ba022acae1f7769fbd46ab6d7fee11466f74d
Vinderguy <vinderguy@gmail.com>
2022-08-10 19:25:49 +0200

    Add _whitelistedCountries Field to AntiVPN Class

--- src/main/java/com/vinderguy/antivpn/AntiVPN.java
+++ src/main/java/com/vinderguy/antivpn/AntiVPN.java
@@ -2,7 +2,11 @@ package com.vinderguy.antivpn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashSet;
+
 public final class AntiVPN extends JavaPlugin {
+
+    private final HashSet<String> _whitelistedCountries = new HashSet<>();
 
     @Override
     public void onEnable() {
