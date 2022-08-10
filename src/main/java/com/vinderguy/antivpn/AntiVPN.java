4ade6d6067549a578457cd2fac9185f690a56609
Vinderguy <vinderguy@gmail.com>
2022-08-10 18:48:53 +0200

    Initial Commit

---
+++ src/main/java/com/vinderguy/antivpn/AntiVPN.java
@@ -0,0 +1,11 @@
+package com.vinderguy.antivpn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiVPN extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
