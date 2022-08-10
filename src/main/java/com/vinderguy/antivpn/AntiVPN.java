ec51d3c70da14dccc9c46e6438cebd11d89b4ca2
Vinderguy <vinderguy@gmail.com>
2022-08-10 21:20:09 +0200

    Add isIPBlocked Method to AntiVPN Class

--- src/main/java/com/vinderguy/antivpn/AntiVPN.java
+++ src/main/java/com/vinderguy/antivpn/AntiVPN.java
@@ -1,8 +1,13 @@
 package com.vinderguy.antivpn;
 
+import com.google.gson.JsonParser;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashSet;
 
 public final class AntiVPN extends JavaPlugin {
@@ -74,6 +79,30 @@ public final class AntiVPN extends JavaPlugin {
         if (_alwaysAllowedIPs.remove(ip)) {
             getConfig().set("always-allowed-ips", _alwaysAllowedIPs);
             saveConfig();
+        }
+    }
+
+    public boolean isIPBlocked(@NotNull final String ip) {
+        if (isIPAlwaysAllowed(ip)) return false;
+
+        try {
+            final var config = getConfig();
+            final var request = new URL(String.format("https://vpnapi.io/api/%s?key=%s", ip, config.getString("api-key"))).openConnection();
+
+            request.connect();
+
+            final var rootElement = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent())).getAsJsonObject();
+
+            final var securityElement = rootElement.get("security").getAsJsonObject();
+            if (config.getBoolean("block-vpn") && securityElement.get("vpn").getAsBoolean()) return true;
+            if (config.getBoolean("block-proxy") && securityElement.get("proxy").getAsBoolean()) return true;
+            if (config.getBoolean("block-tor") && securityElement.get("tor").getAsBoolean()) return true;
+            if (config.getBoolean("block-relay") && securityElement.get("relay").getAsBoolean()) return true;
+
+            final var country = rootElement.get("location").getAsJsonObject().get("country_code").getAsString();
+            return (config.getBoolean("enable-country-whitelist") && !isCountryWhitelisted(country) || config.getBoolean("enable-country-blacklist") && isCountryBlacklisted(country));
+        } catch (IOException e) {
+            return false;
         }
     }
 }
