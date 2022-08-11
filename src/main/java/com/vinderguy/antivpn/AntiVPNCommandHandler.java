096577607ba0cfb9b1d6e1914290e88e4f254fc6
Vinderguy <vinderguy@gmail.com>
2022-08-11 20:28:58 +0200

    Rename sendPredefinedChatMessage Method to sendPredefinedMessage

--- src/main/java/com/vinderguy/antivpn/AntiVPNCommandHandler.java
+++ src/main/java/com/vinderguy/antivpn/AntiVPNCommandHandler.java
@@ -21,35 +21,35 @@ public final class AntiVPNCommandHandler implements CommandExecutor, TabComplete
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 0) {
-            if (_plugin.isProtectionEnabled()) sendPredefinedChatMessage(sender, "status-enabled");
-            else sendPredefinedChatMessage(sender, "status-disabled");
+            if (_plugin.isProtectionEnabled()) sendPredefinedMessage(sender, "status-enabled");
+            else sendPredefinedMessage(sender, "status-disabled");
 
             return true;
         }
 
         if (args[0].equalsIgnoreCase("enable")) {
             _plugin.setProtectionEnabled(true);
-            sendPredefinedChatMessage(sender, "enabled");
+            sendPredefinedMessage(sender, "enabled");
 
             return true;
         }
 
         if (args[0].equalsIgnoreCase("disable")) {
             _plugin.setProtectionEnabled(false);
-            sendPredefinedChatMessage(sender, "disabled");
+            sendPredefinedMessage(sender, "disabled");
 
             return true;
         }
 
         if (args[0].equalsIgnoreCase("exempt")) {
             if (args.length == 1) {
-                sendPredefinedChatMessage(sender, "exempt-unknown-action");
+                sendPredefinedMessage(sender, "exempt-unknown-action");
                 return true;
             }
 
             if (args[1].equalsIgnoreCase("add")) {
                 if (args.length == 2) {
-                    sendPredefinedChatMessage(sender, "exempt-add-unknown-ip");
+                    sendPredefinedMessage(sender, "exempt-add-unknown-ip");
                     return true;
                 }
 
@@ -60,7 +60,7 @@ public final class AntiVPNCommandHandler implements CommandExecutor, TabComplete
 
             if (args[1].equalsIgnoreCase("remove")) {
                 if (args.length == 2) {
-                    sendPredefinedChatMessage(sender, "exempt-remove-unknown-ip");
+                    sendPredefinedMessage(sender, "exempt-remove-unknown-ip");
                     return true;
                 }
 
@@ -69,13 +69,13 @@ public final class AntiVPNCommandHandler implements CommandExecutor, TabComplete
                 return true;
             }
 
-            sendPredefinedChatMessage(sender, "exempt-unknown-action");
+            sendPredefinedMessage(sender, "exempt-unknown-action");
             return true;
         }
 
         if (args[0].equalsIgnoreCase("reload")) {
             _plugin.reload();
-            sendPredefinedChatMessage(sender, "reloaded");
+            sendPredefinedMessage(sender, "reloaded");
 
             return true;
         }
@@ -108,7 +108,7 @@ public final class AntiVPNCommandHandler implements CommandExecutor, TabComplete
         return Collections.emptyList();
     }
 
-    private void sendPredefinedChatMessage(@NotNull final CommandSender recipient, @NotNull final String name) {
+    private void sendPredefinedMessage(@NotNull final CommandSender recipient, @NotNull final String name) {
         recipient.sendMessage(_plugin.getMessage(name));
     }
 }
